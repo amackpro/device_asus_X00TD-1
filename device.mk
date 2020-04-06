923039e365fb10be95f623459ad05bb1f9463a95
thetoymaker <toymaker4ever@gmail.com>
2020-04-06 23:33:03 +0530

    X00TD: Don't build Doze for now

--- device.mk
+++ device.mk
@@ -203,8 +203,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # Doze mode
-PRODUCT_PACKAGES += \
-    Doze
+#PRODUCT_PACKAGES += \
+#    Doze
 
 # FM
 PRODUCT_PACKAGES += \
